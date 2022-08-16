2a97ce17a39f59a932539b26fefffc994d2a493f
jaybk051183 <kailashbhaggoe@outlook.com>
2022-08-16 14:04:58 +0200

    update json

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-console.log('Hallo daar!');
+import axios from "axios";
+
+async function fetchCountries(){
+    try{
+        const result = await axios.get(encodeURI("https://restcountries.com/v2/all"));
+        for (let i = 0; i < 250; i++) {
+
+        }
+        console.log(result.data);
+
+    }
+    catch(e) {
+        console.error(e)
+    }
+
+}
+
+fetchCountries()
+
+
+
